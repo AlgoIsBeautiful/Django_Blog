25e853dc8e8962932bed747f3c89b81cebec5eed
AlgoIsBeautiful <rwang432@gatech.edu>
2019-10-07 17:37:03 -0500

    nice day

--- BlogProject/blog/urls.py
+++ BlogProject/blog/urls.py
@@ -2,7 +2,9 @@ from django.conf.urls import url
 
 from . import views
 
+# below app_name tells Django that this urls.py belongs to 'blog'
 app_name = 'blog'
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^post/(?P<pk>[0-9]+)/$', views.detail, name='detail'), # [0-9]+ represents 1 or more digits /1/, /255/
