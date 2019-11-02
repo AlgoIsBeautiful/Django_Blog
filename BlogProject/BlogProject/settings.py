9d1f20f34c62a690566e8c95d7a71d4d4d682824
AlgoIsBeautiful <rwang432@gatech.edu>
2019-11-02 17:54:02 -0500

    add domain info

--- BlogProject/BlogProject/settings.py
+++ BlogProject/BlogProject/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ewkk*7s345td+!8d=2gga#1=4c!030dee#c0y3l)unok8yht^w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = ['www.datasciencegeek.dev']
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.datasciencegeek.dev']
+ALLOWED_HOSTS = ['52.70.205.165', 'www.datasciencegeek.dev', '.datasciencegeek.dev']
 
 # Application definition
 
